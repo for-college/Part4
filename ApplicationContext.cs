1faed310fd858738be868a9a8fbd4760d71c7596
scffs <scffs@KOMPUTER>
2024-01-29 12:20:51 +0700

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Part4
+{
+    public class ApplicationContext : DbContext
+    {
+        public string connectionString = "";
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using Part4;
+
+namespace Part4
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int CompanyId { get; set; }
+        public Company? Company { get; set; }
+    }
+}
